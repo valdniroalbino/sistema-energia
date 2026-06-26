ece464cca22db8d1f19bd9646363b7a88e4eb0f2
Valdniro Albino <Leopoldinoalbino12@hotmail.com>
2026-06-26 12:57:43 +0100

    menu inicial

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "grafo.h"
+#include "io.h"
+
+int main(void) {
+    Grafo g;
+    g.vertices = NULL;
+    g.arestas  = NULL;
+    g.n_verti  = 0;
+    g.n_arest  = 0;
+
+    int opcao;
+
+    do {
+        printf("\n===== Sistema de Rede Electrica =====\n");
+        printf("1. Carregar rede do ficheiro\n");
+        printf("2. Mostrar grafo carregado\n");
+        printf("0. Sair\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 1:
+                carregaRede(&g, "rede_electrica.txt");
+                break;
+            case 2:
+                /* TODO: chamar mostrarGrafo quando Josemar implementar */
+                printf("Vertices: %d | Ligacoes: %d\n", g.n_verti, g.n_arest);
+                break;
+            case 0:
+                printf("A sair...\n");
+                break;
+            default:
+                printf("Opcao invalida.\n");
+        }
+
+    } while(opcao != 0);
+
+    return 0;
+}
